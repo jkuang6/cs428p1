31b493b8b42adf2d8c05963a1f9fa4dd66d28279
Jia <jkuang6@uic.edu>
2020-09-11 00:51:49 -0500

    project 1

---
+++ Assets/Date.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Date : MonoBehaviour
+{
+    public GameObject dateObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dateObject.GetComponent<TextMeshPro>().text = System.DateTime.Now.ToString("MM/dd/yyyy");
+    }
+
+}
